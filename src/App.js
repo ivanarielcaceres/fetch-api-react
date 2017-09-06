571c060ca8f672168777cc8ae145ea59d5c21e2c
Caceres Cañete <Caceres Caete, Ivan Ariel Dimas>
2017-09-06 10:36:04 -0300

    changed columns order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import {
   Table,
   TableBody,
@@ -23,7 +24,10 @@ class App extends Component {
     return fetch('https://account.monitorank.com/api.php?key=2201-sBD0Lh4gzaCO0cpYpllA2CcWXBlNmy8FZBwAJqH8&module=entity&action=get&id_entity=17631&period=20170701-20170724')
     .then((response) => response.json())
     .then((responseJson) => {
-      this.setState({data: responseJson.data})
+      let sorted = responseJson.data.sort((a, b) => {
+          return a.query.id - b.query.id;
+      });
+      this.setState({data: sorted})
     })
     .catch((error) => {
       console.error(error);
@@ -49,22 +53,22 @@ class App extends Component {
         <Table onRowSelection={this.handleRowSelection.bind(this)} style={{ tableLayout: 'auto'}} fixedHeader={false}>
           <TableHeader displaySelectAll ={false}>
             <TableRow>
-              <TableHeaderColumn>Url</TableHeaderColumn>
               <TableHeaderColumn>Entity</TableHeaderColumn>
-              <TableHeaderColumn>Keywords</TableHeaderColumn>
-              <TableHeaderColumn>Type Rank</TableHeaderColumn>
-
-              <TableHeaderColumn>Query id</TableHeaderColumn>
-              <TableHeaderColumn>Query date</TableHeaderColumn>
-              <TableHeaderColumn>Query device</TableHeaderColumn>
-              <TableHeaderColumn>Query nb top</TableHeaderColumn>
               <TableHeaderColumn>Query country</TableHeaderColumn>
               <TableHeaderColumn>Query service</TableHeaderColumn>
               <TableHeaderColumn>Query version</TableHeaderColumn>
               <TableHeaderColumn>Query language</TableHeaderColumn>
-              <TableHeaderColumn>Query localisation</TableHeaderColumn>
               <TableHeaderColumn>Query keywords</TableHeaderColumn>
+              <TableHeaderColumn>Query id</TableHeaderColumn>
+              <TableHeaderColumn>Query date</TableHeaderColumn>
+              <TableHeaderColumn>Query device</TableHeaderColumn>
+              <TableHeaderColumn>Query nb top</TableHeaderColumn>
+              <TableHeaderColumn>Query localisation</TableHeaderColumn>
               <TableHeaderColumn>Query volume</TableHeaderColumn>
+              <TableHeaderColumn>Url</TableHeaderColumn>
+
+              <TableHeaderColumn>Keywords</TableHeaderColumn>
+              <TableHeaderColumn>Type Rank</TableHeaderColumn>
 
               <TableHeaderColumn>Rank 2017-07-01</TableHeaderColumn>
               <TableHeaderColumn>Rank 2017-07-02</TableHeaderColumn>
@@ -96,22 +100,23 @@ class App extends Component {
             {this.state.data.map((row, i) => (
                 <TableRow selected={this.isSelected(0)}>
                   <TableRowColumn>{i}</TableRowColumn>
-                  <TableRowColumn>{row.url}</TableRowColumn>
-                  <TableRowColumn>{row.entity}</TableRowColumn>
-                  <TableRowColumn>{row.keywords}</TableRowColumn>
-                  <TableRowColumn>{row.type_rank}</TableRowColumn>
 
-                  <TableRowColumn>{row.query.id}</TableRowColumn>
-                  <TableRowColumn>{row.query.date}</TableRowColumn>
-                  <TableRowColumn>{row.query.device}</TableRowColumn>
-                  <TableRowColumn>{row.query.nb_top}</TableRowColumn>
+                  <TableRowColumn>{row.entity}</TableRowColumn>
                   <TableRowColumn>{row.query.country}</TableRowColumn>
                   <TableRowColumn>{row.query.service}</TableRowColumn>
                   <TableRowColumn>{row.query.version}</TableRowColumn>
                   <TableRowColumn>{row.query.language}</TableRowColumn>
-                  <TableRowColumn>{row.query.localisation}</TableRowColumn>
                   <TableRowColumn>{row.query.keywords}</TableRowColumn>
+                  <TableRowColumn>{row.query.id}</TableRowColumn>
+                  <TableRowColumn>{row.query.date}</TableRowColumn>
+                  <TableRowColumn>{row.query.device}</TableRowColumn>
+                  <TableRowColumn>{row.query.nb_top}</TableRowColumn>
+                  <TableRowColumn>{row.query.localisation}</TableRowColumn>
                   <TableRowColumn>{row.query.volume}</TableRowColumn>
+                  <TableRowColumn>{row.url}</TableRowColumn>
+
+                  <TableRowColumn>{row.keywords}</TableRowColumn>
+                  <TableRowColumn>{row.type_rank}</TableRowColumn>
 
                   <TableRowColumn>{row.rank['2017-07-01']}</TableRowColumn>
                   <TableRowColumn>{row.rank['2017-07-02']}</TableRowColumn>
